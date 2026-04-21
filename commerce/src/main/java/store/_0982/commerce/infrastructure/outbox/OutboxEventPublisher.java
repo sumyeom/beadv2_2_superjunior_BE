adc7981af27e2a1c4fd36e311947ba16bb815044
sumyeom <wjstndus75@naver.com>
2026-04-21 09:37:28 +0900

    feat: outbox 토글 변경

--- commerce/src/main/java/store/_0982/commerce/infrastructure/outbox/OutboxEventPublisher.java
+++ commerce/src/main/java/store/_0982/commerce/infrastructure/outbox/OutboxEventPublisher.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(prefix = "outbox.publisher", name = "enabled", havingValue = "true", matchIfMissing = true)
 @RequiredArgsConstructor
 public class OutboxEventPublisher {
     private final OutboxEventService outboxEventService;
